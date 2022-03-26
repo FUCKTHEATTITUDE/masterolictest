26a92eed4c0de4719322d1ed29de3ccc09904790
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-03-26 18:51:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning Repo, Please Wait..."
-  git clone -b master https://github.com/Naveen-TG/Dhanush.git /Dhanush
+  git clone -b master https://github.com/Naveen-TG/Masterolic.git /Masterolic
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Dhanush
+  git clone $UPSTREAM_REPO /Masterolic
 fi
 cd /Dhanush 
 echo "Installing Requirements..."
